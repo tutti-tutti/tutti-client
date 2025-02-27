201119087d75b38e3cd3274686190638a0dde418
Boongranii <jobc4550@gmail.com>
2025-02-27 19:13:41 +0900

    [⚙️ chore] 프로젝트 셋팅 (#5) (#6)
    * chore: init project (#5)
    
    * chore: tailwind css 버전업 (#5)
    
    * chore: 주요 라이브러리 설치 (#5)

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "../styles/globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Tutti",
+  description: "Tutti의 프론트엔드입니다.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

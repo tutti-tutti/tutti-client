bc2fa1252142fbc1a15f4ce8848486ad86d3a86e
bbjbc <jobc4550@gmail.com>
2025-02-27 19:01:51 +0900

    chore: tailwind css 버전업 (#5)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 const HomePage = () => {
-  return <h1>반갑습니다. Tutti입니다.</h1>;
+  return (
+    <h1 className="text-xl font-extrabold text-blue-700">
+      반갑습니다. Tutti입니다.
+    </h1>
+  );
 };
 
 export default HomePage;

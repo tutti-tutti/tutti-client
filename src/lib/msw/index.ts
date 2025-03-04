51265af2330aaa0fb801c40ac1af9329db622646
vgotu99 <vgotu99@gmail.com>
2025-03-04 17:48:38 +0900

    remove: 클라이언트 사이드에서 api route만을 호출하도록 msw의 worker.ts 제거 (#16)

--- src/lib/msw/index.ts
+++ src/lib/msw/index.ts
@@ -1,9 +1,6 @@
 export const initMsw = async () => {
-  if (typeof window === 'undefined') {
-    const { server } = await import('./server');
-    server.listen();
-  } else {
-    const { worker } = await import('./worker');
-    await worker.start();
-  }
+  if (typeof window !== 'undefined') return;
+
+  const { server } = await import('./server');
+  server.listen();
 };

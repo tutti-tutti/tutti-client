24f6c29c38a4f71f8fdeec75a7573240a4165675
gold <goldegg127@gmail.com>
2025-03-05 23:16:38 +0900

    feat: padding 디자인 토큰 정의 (#15)

---
+++ src/styles/token/padding.ts
@@ -0,0 +1,28 @@
+const padding = {
+  xs2: '4px',
+  xs: '8px',
+  sm: '12px',
+  md: '16px',
+  lg: '24px',
+  xl: '28px',
+  xl2: '32px',
+  xl3: '36px',
+  xl4: '48px',
+  xl5: '60px',
+  xl6: '80px',
+  values: {
+    xxs: 4,
+    xs: 8,
+    sm: 12,
+    md: 16,
+    lg: 24,
+    xl: 28,
+    xl2: 32,
+    xl3: 36,
+    xl4: 48,
+    xl5: 60,
+    xl6: 80,
+  },
+};
+
+export { padding };

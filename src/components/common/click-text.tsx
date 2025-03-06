ad7da352a2a0472af15a1115a929573a07fd8cf2
bbjbc <jobc4550@gmail.com>
2025-03-06 15:27:04 +0900

    feat: click text 컴포넌트 생성 및 스토리북 작성 (#39)

---
+++ src/components/common/click-text.tsx
@@ -0,0 +1,46 @@
+import Link from 'next/link';
+
+import { cn } from '@/utils/cn';
+
+interface ClickTextProps extends React.HTMLAttributes<HTMLButtonElement> {
+  href?: string;
+  disabled?: boolean;
+  className?: string;
+}
+
+const ClickText = ({
+  href,
+  children,
+  disabled = false,
+  className = '',
+  onClick,
+  ...props
+}: ClickTextProps) => {
+  const baseStyle = 'text-base transition-colors duration-300';
+  const stateStyles = disabled
+    ? 'text-gray-400 cursor-not-allowed'
+    : 'text-[#262626] hover:text-[#5D38E8] active:text-[#38228C]';
+
+  if (disabled || !href) {
+    return (
+      <button
+        className={cn(baseStyle, stateStyles, className)}
+        onClick={onClick}
+        disabled={disabled}
+        {...props}
+      >
+        {children}
+      </button>
+    );
+  }
+
+  return (
+    <Link href={href}>
+      <p className={cn(baseStyle, stateStyles, className)} {...props}>
+        {children}
+      </p>
+    </Link>
+  );
+};
+
+export default ClickText;

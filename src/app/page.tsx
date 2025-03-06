185cc23a21bd957b51e885da1e7fd2e12cc501ce
gold <goldegg127@gmail.com>
2025-03-06 12:13:04 +0900

    test: 디자인토큰으로 정의된 유틸클래스 테스트 (#15)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const HomePage = () => {
   return (
-    <h1 className="text-xl font-extrabold text-blue-700">
+    <h1 className="text-xl font-extrabold text-base-white bg-bg-successBold">
       반갑습니다. Tutti입니다.
     </h1>
   );

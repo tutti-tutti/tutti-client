f669749343ed18f487543f2ad57b3305f43f3d81
gold <goldegg127@gmail.com>
2025-03-06 14:06:38 +0900

    feat: padding, margin, gap 관련 spacing 디자인토큰 생성 및 유틸클래스 적용 (#15)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 const HomePage = () => {
   return (
-    <h1 className="text-xl font-extrabold text-base-white bg-bg-successBold">
-      반갑습니다. Tutti입니다.
-    </h1>
+    <div className="mt-2xl mb-xl ml-2xs mr-md flex flex-col gap-md">
+      <h1 className="p-xs text-xl font-extrabold text-base-white bg-bg-successBold">
+        반갑습니다. Tutti입니다.
+      </h1>
+      <p>테스트 입니다.</p>
+    </div>
   );
 };
 

b8b4a411c63d3c6f41092564d9257625f6076004
bbjbc <jobc4550@gmail.com>
2025-03-07 11:24:38 +0900

    design: 인터랙션 디자인 토큰 적용 (#43)

--- src/components/common/NavIcon.tsx
+++ src/components/common/NavIcon.tsx
@@ -9,7 +9,7 @@ interface NavIconProps {
 const NavIcon = ({ href, icon, label }: NavIconProps) => {
   return (
     <Link href={href}>
-      <div className="gap-2xs transition-300 text-primary flex cursor-pointer flex-col items-center text-sm transition-colors duration-300 hover:text-[#5D38E8] active:text-[#38228C]">
+      <div className="gap-2xs transition-300 text-text-primary hover:text-text-primaryHover active:text-text-primaryPressed flex cursor-pointer flex-col items-center text-sm transition-colors duration-300">
         <div>{icon}</div>
         <span className="text-sm whitespace-nowrap" aria-label={label}>
           {label}

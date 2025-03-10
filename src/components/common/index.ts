9bfa01d22b239b20743757be95727f26576a10cb
seunggeon <vgotu99@naver.com>
2025-03-10 11:54:41 +0900

    [✨ feat] 인풋 컴포넌트 구현 (#56)
    * feat: 인풋 컴포넌트 구현 (#49)
    
    * design: 비활성화 상태에 대한 디자인 추가 (#49)
    
    * feat: barrel export 패턴 적용 (#49)
    
    * feat: 인풋 컴포넌트 스토리북 추가 (#49)
    
    * refactor: 컴포넌트 타입을 직접 정의하는 방식으로 변경 (#49)

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export { default as Button } from './button/Button';
 export { default as ClickTextButton } from './button/ClickTextButton';
 export { default as IconButton } from './button/IconButton';
 export * from './header';
+export { default as Input } from './Input';

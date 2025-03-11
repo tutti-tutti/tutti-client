57d0aec9b40785ab4e121cabdb4464714de01cd4
vgotu99 <vgotu99@gmail.com>
2025-03-11 10:29:44 +0900

    design: (auth) 라우팅 그룹의 레이아웃이 중앙에 배치되도록 수정 (#60)

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,8 @@
-import { JihyeIcon } from '@/assets';
-import { ROUTER_PATH } from '@/constants';
+import React, { type ReactNode } from 'react';
 import Link from 'next/link';
-import React, { ReactNode } from 'react';
+
+import { Icon } from '@/components';
+import { ROUTER_PATH } from '@/constants';
 
 interface AuthLayoutProps {
   children: ReactNode;
@@ -9,15 +10,17 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div className="m-5xl p-6xl shadow-cast h-full w-[632px] rounded-2xl shadow-[0px_var(--effect-shadow-8-cast-y,_4px)_var(--effect-shadow-8-cast-blur,_8px)_0px_var(--effect-shadow-cast,_rgba(0,_0,_0,_0.16)),_0px_var(--effect-shadow-8-core-y,_0px)_var(--effect-shadow-8-core-blur,_4px)_0px_var(--effect-shadow-core,_rgba(0,_0,_0,_0.12))]">
-      <div className="flex w-full flex-col">
-        <Link
-          className="p-sm mb-2xl flex justify-center"
-          href={ROUTER_PATH.HOME}
-        >
-          <JihyeIcon />
-        </Link>
-        {children}
+    <div className="flex justify-center">
+      <div className="m-5xl p-6xl shadow-cast flex h-full w-[632px] items-center justify-center rounded-2xl shadow-[0px_var(--effect-shadow-8-cast-y,_4px)_var(--effect-shadow-8-cast-blur,_8px)_0px_var(--effect-shadow-cast,_rgba(0,_0,_0,_0.16)),_0px_var(--effect-shadow-8-core-y,_0px)_var(--effect-shadow-8-core-blur,_4px)_0px_var(--effect-shadow-core,_rgba(0,_0,_0,_0.12))]">
+        <div className="flex w-full flex-col">
+          <Link
+            className="p-sm mb-2xl flex justify-center"
+            href={ROUTER_PATH.HOME}
+          >
+            <Icon iconName="comment" />
+          </Link>
+          {children}
+        </div>
       </div>
     </div>
   );

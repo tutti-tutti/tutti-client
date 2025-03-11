680bca318876fe3c614e203df1cf3bf4880d3c41
bbjbc <jobc4550@gmail.com>
2025-03-11 16:01:54 +0900

    refactor: icon button 컴포넌트 범용성 높이도록 수정 (#75)

--- src/components/common/button/IconButton.tsx
+++ src/components/common/button/IconButton.tsx
@@ -1,19 +1,29 @@
-import Button, { type ButtonVariant } from '@/components/common/button/Button';
+import { cn } from '@/utils';
 import type { IconType } from '@/types';
+import Button, { type ButtonVariant } from './Button';
 
 export interface IconButtonProps {
   icon: IconType;
   variant?: ButtonVariant;
   onClick: () => void;
+  iconProps?: React.SVGProps<SVGSVGElement>;
+  className?: string;
 }
 
-const IconButton = ({ icon, variant, onClick }: IconButtonProps) => {
+const IconButton = ({
+  icon,
+  variant,
+  onClick,
+  iconProps,
+  className,
+}: IconButtonProps) => {
   return (
     <Button
       icon={icon}
+      iconProps={iconProps}
       variant={variant}
       onClick={onClick}
-      className="p-xs rounded-full"
+      className={cn('!p-xs rounded-full', className)}
     />
   );
 };

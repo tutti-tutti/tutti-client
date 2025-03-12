87fe1560ccb9ff281a41c3053ff222739632f331
gold <goldegg127@gmail.com>
2025-03-13 03:14:25 +0900

    refactor: 컴포넌트 NavIcon 의 재사용성을 위해 Link 컴포넌트 분리 (#81)

--- src/components/layout/header/HeaderNavigation.tsx
+++ src/components/layout/header/HeaderNavigation.tsx
@@ -1,25 +1,21 @@
-import { NavIcon, NationIcon, CartIcon, UserIcon } from '@/components';
+import Link from 'next/link';
+
 import { ROUTER_PATH, PATH_NAME } from '@/constants';
+import { NavIcon, NationIcon, CartIcon, UserIcon } from '@/components';
 
 const HeaderNavigation = () => {
   return (
-    <div className="gap-xl flex">
-      <NavIcon
-        href={ROUTER_PATH.COUNTRY_SETTING}
-        icon={<NationIcon />}
-        label={PATH_NAME.COUNTRY_SETTING}
-      />
-      <NavIcon
-        href={ROUTER_PATH.CART}
-        icon={<CartIcon />}
-        label={PATH_NAME.CART}
-      />
-      <NavIcon
-        href={ROUTER_PATH.MY_PAGE}
-        icon={<UserIcon />}
-        label={PATH_NAME.MY_PAGE}
-      />
-    </div>
+    <nav className="md:gap-xl gap-md flex">
+      <Link href={ROUTER_PATH.COUNTRY_SETTING}>
+        <NavIcon icon={<NationIcon />} label={PATH_NAME.COUNTRY_SETTING} />
+      </Link>
+      <Link href={ROUTER_PATH.CART}>
+        <NavIcon icon={<CartIcon />} label={PATH_NAME.CART} />
+      </Link>
+      <Link href={ROUTER_PATH.MY_PAGE}>
+        <NavIcon icon={<UserIcon />} label={PATH_NAME.MY_PAGE} />
+      </Link>
+    </nav>
   );
 };
 

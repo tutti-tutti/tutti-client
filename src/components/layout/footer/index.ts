0d7f8c7a60be1a396cad1f89867e2946e87bc0ff
vgotu99 <vgotu99@gmail.com>
2025-03-12 15:14:29 +0900

    fix: 삭제된 컴포넌트를 export하는 구문 삭제 (#60)

--- src/components/layout/footer/index.ts
+++ src/components/layout/footer/index.ts
@@ -1,6 +1,6 @@
 export { default as Footer } from './Footer';
 export { default as FooterCompanyInfo } from './FooterCompanyInfo';
 export { default as FooterCustomerInfo } from './FooterCustomerInfo';
-export { default as FooterPaymentInfo } from './FooterPaymentInfo';
+// export { default as FooterPaymentInfo } from './FooterPaymentInfo';
 export { default as FooterLogo } from './FooterLogo';
 export { default as FooterNav } from './FooterNav';

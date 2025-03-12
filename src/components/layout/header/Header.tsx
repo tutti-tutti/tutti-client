55981b256cb86474166b7196433eef9dd319275b
gold <goldegg127@gmail.com>
2025-03-13 03:15:15 +0900

    design: header 반응형 UI 적용 (#81)

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,15 +1,15 @@
 import Link from 'next/link';
 
-import { SearchInput, JihyeIcon } from '@/components';
 import { ROUTER_PATH } from '@/constants';
+import { SearchInput, JihyeIcon, SearchIcon, NavIcon } from '@/components';
 import HeaderNavigation from './HeaderNavigation';
 import TextHeader from './TextHeader';
 
 const Header = () => {
   return (
-    <header className="px-md pt-2xl pb-5xl border-border-secondary w-full border-b">
+    <header className="px-md pt-lg md:pt-2xl md:pb-5xl md:border-border-secondary w-full md:border-b">
       <div className="gap-2xl mx-auto flex w-full max-w-[1280px] flex-col">
-        <nav className="ml-auto">
+        <nav className="ml-auto hidden md:block">
           <TextHeader
             country="한국"
             isLoggedIn={false}
@@ -17,12 +17,19 @@ const Header = () => {
           />
         </nav>
 
-        <div className="gap-lg flex h-[84px] items-center">
+        <div className="gap-lg flex h-[54px] items-center justify-between md:h-[84px]">
           <Link href={ROUTER_PATH.HOME}>
-            <JihyeIcon />
+            <JihyeIcon className="w-[104px] md:w-[208px]" />
           </Link>
-          <SearchInput />
-          <HeaderNavigation />
+          <div className="hidden md:block">
+            <SearchInput />
+          </div>
+          <div className="md:gap-xl gap-md flex">
+            <HeaderNavigation />
+            <button type="button" className="block md:hidden">
+              <NavIcon icon={<SearchIcon />} label="검색" />
+            </button>
+          </div>
         </div>
       </div>
     </header>

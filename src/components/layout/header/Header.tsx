66920fb58dd0b41b6ca42ce20af32009e8827688
gold <goldegg127@gmail.com>
2025-03-14 00:00:29 +0900

    feat: 모바일 검색창 오픈 UI 임시 적용 (#81)

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
 
 import { ROUTER_PATH } from '@/constants';
 import { SearchInput, Logo, SearchIcon, NavIcon } from '@/components';
@@ -6,9 +9,16 @@ import HeaderNavigation from './HeaderNavigation';
 import TextHeader from './TextHeader';
 
 const Header = () => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const toggleSearch = () => {
+    setIsSearchOpen(!isSearchOpen);
+    console.log('isSearchOpen', isSearchOpen);
+  };
+
   return (
-    <header className="px-container pt-lg md:pt-2xl md:pb-5xl md:border-border-secondary w-full md:border-b">
-      <div className="gap-2xl mx-auto flex w-full max-w-[1280px] flex-col">
+    <header className="bg-bg-primary px-container pt-lg md:pt-2xl md:pb-5xl md:border-border-secondary w-full md:border-b">
+      <div className="gap-lg mx-auto flex w-full max-w-[1280px] flex-col">
         <nav className="ml-auto hidden md:block">
           <TextHeader
             country="한국"
@@ -17,16 +27,25 @@ const Header = () => {
           />
         </nav>
 
-        <div className="gap-lg flex h-[54px] items-center justify-between md:h-[84px]">
+        <div className="gap-lg flex h-[54px] items-center justify-between md:h-[64px]">
           <Link href={ROUTER_PATH.HOME}>
             <Logo />
           </Link>
           <div className="hidden flex-1 md:block">
             <SearchInput />
           </div>
+          {isSearchOpen && (
+            <div className="bg-bg-primary absolute right-[var(--space-container)] left-[var(--space-container)] h-[64px] w-[calc(100%-2*var(--space-container))] md:hidden">
+              <SearchInput />
+            </div>
+          )}
           <div className="md:gap-xl gap-md flex">
             <HeaderNavigation />
-            <button type="button" className="block md:hidden">
+            <button
+              type="button"
+              className="block md:hidden"
+              onClick={toggleSearch}
+            >
               <NavIcon icon={<SearchIcon />} label="검색" />
             </button>
           </div>

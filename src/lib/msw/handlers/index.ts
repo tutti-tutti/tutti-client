32263ae935c4aa9cde1b2169d4b9ce1d9274ae98
bbjbc <jobc4550@gmail.com>
2025-03-16 23:15:41 +0900

    feat: 상품 상세에 대한 핸들러 추가 (#100)

--- src/lib/msw/handlers/index.ts
+++ src/lib/msw/handlers/index.ts
@@ -2,10 +2,12 @@ import { userHandlers } from './userHandlers';
 import {
   productsHandlers,
   recommendedProductsHandlers,
+  productDetailHandlers,
 } from './productsHandlers';
 
 export const handlers = [
   ...userHandlers,
   ...productsHandlers,
   ...recommendedProductsHandlers,
+  ...productDetailHandlers,
 ];
--- src/lib/msw/handlers/productsHandlers.ts
+++ src/lib/msw/handlers/productsHandlers.ts
@@ -14,3 +14,24 @@ export const recommendedProductsHandlers = [
     return HttpResponse.json(products);
   }),
 ];
+
+export const productDetailHandlers = [
+  http.get(PRODUCTS_ENDPOINTS.DETAIL, ({ params }) => {
+    const { productId } = params;
+    let product;
+    products.forEach(p => {
+      const foundProduct = p.latestList.find(
+        item => item.productId.toString() === productId,
+      );
+      if (foundProduct) {
+        product = foundProduct;
+      }
+    });
+
+    if (!product) {
+      return new HttpResponse(null, { status: 404 });
+    }
+
+    return HttpResponse.json(product);
+  }),
+];

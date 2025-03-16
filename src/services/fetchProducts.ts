4f942d48aa01f2873626cf24b659dddd37f8d5e1
bbjbc <jobc4550@gmail.com>
2025-03-16 23:16:49 +0900

    feat: productId 값에 따라 상품 정보를 가져오는 서비스 함수 추가 (#100)

--- src/services/fetchProducts.ts
+++ src/services/fetchProducts.ts
@@ -1,5 +1,6 @@
 import { axiosInstance } from '@/lib/axios';
 import { PRODUCT_PATHS } from '@/constants';
+import { Product } from '@/types';
 
 export const fetchProducts = async () => {
   const response = await axiosInstance.get(PRODUCT_PATHS.LIST);
@@ -9,6 +10,13 @@ export const fetchProducts = async () => {
 
 export const fetchRecommededProducts = async () => {
   const response = await axiosInstance.get(PRODUCT_PATHS.RECOMMEND);
+
+  return response.data;
+};
+
+export const fetchProductById = async (productId: string): Promise<Product> => {
+  const endpoint = PRODUCT_PATHS.DETAIL.replace(':productId', productId);
+  const response = await axiosInstance.get(endpoint);
 
   return response.data;
 };

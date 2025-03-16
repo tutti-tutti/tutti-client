59d442eb2759419c07afdc72e788f6cf91d31eaf
bbjbc <jobc4550@gmail.com>
2025-03-16 22:46:55 +0900

    design: radio icon에서 fill 색상 토큰에 정의된 색상으로 수정 (#100)

--- src/components/icons/RadioIcon.tsx
+++ src/components/icons/RadioIcon.tsx
@@ -11,7 +11,7 @@ const RadioIcon = (props: React.SVGProps<SVGSVGElement>) => {
     >
       <path
         d="M0 12C0 5.37258 5.37258 0 12 0C18.6274 0 24 5.37258 24 12C24 18.6274 18.6274 24 12 24C5.37258 24 0 18.6274 0 12Z"
-        fill="#663EFF"
+        fill="var(--color-violet-500)"
       />
       <circle cx="12" cy="12" r="6" fill="white" />
     </svg>

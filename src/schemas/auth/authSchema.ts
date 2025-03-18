9f178bda8433b4099f511c14bf1c77153c9bf3a1
vgotu99 <vgotu99@gmail.com>
2025-03-18 15:19:36 +0900

    refactor: zod로 비밀번호 유효성 검사 시 사용하는 정규식을 의미있는 상수명으로 선언하여 적용 (#98)

--- src/schemas/auth/authSchema.ts
+++ src/schemas/auth/authSchema.ts
@@ -9,17 +9,15 @@ const {
   CHECK_POLICY: { ESSENTIALS },
 } = AUTH_CONSTANTS;
 
+const PASSWORD_LOWERCASE_NUMBER_MIN8_REGEX =
+  /^(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,}$/;
+
 export const authSchema = z.object({
   email: z.string().email({ message: EMAIL_INPUT.ERROR }).trim(),
   verifyEmail: z.string().min(1, { message: VERIFY_EMAIL_INPUT.ERROR }),
-  pw: z
-    .string()
-    .regex(
-      /^(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]{8,}$/,
-      {
-        message: PW_INPUT.ERROR,
-      },
-    ),
+  pw: z.string().regex(PASSWORD_LOWERCASE_NUMBER_MIN8_REGEX, {
+    message: PW_INPUT.ERROR,
+  }),
   checkPw: z.string(),
   essentialPolicy: z
     .array(z.string())

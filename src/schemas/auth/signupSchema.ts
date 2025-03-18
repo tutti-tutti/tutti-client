82c09a275cfdaea6896d0cc381f15d49a9ab2927
vgotu99 <vgotu99@gmail.com>
2025-03-18 17:26:52 +0900

    hotfix: 동등한 레벨의 경로에 있는 파일의 코드를 최상위 경로에서 import하지 않도록 수정 (#111)

--- src/schemas/auth/signupSchema.ts
+++ src/schemas/auth/signupSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 import { AUTH_CONSTANTS } from '@/constants';
-import { authSchema } from '@/schemas';
+import { authSchema } from '@/schemas/auth/authSchema';
 
 const { CHECK_PW_INPUT } = AUTH_CONSTANTS;
 

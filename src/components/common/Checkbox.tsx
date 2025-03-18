103b11e621826ed1fbef565639acf0a9abe2ffd6
bbjbc <jobc4550@gmail.com>
2025-03-18 17:38:11 +0900

    feat: 체크박스 컴포넌트 제작 및 스토리북 작성 (#110)

---
+++ src/components/common/Checkbox.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { Icon } from '@/components';
+import { cn } from '@/utils';
+
+interface CheckboxProps {
+  label?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  onChange?: (checked: boolean) => void;
+  className?: string;
+}
+
+const Checkbox = ({
+  label,
+  checked = false,
+  disabled = false,
+  onChange,
+  className = '',
+}: CheckboxProps) => {
+  const checkboxBaseClass =
+    'p-2xs flex h-[var(--space-lg)] w-[var(--space-lg)] items-center justify-center rounded-md border-[1.5px] transition-colors duration-300';
+
+  const checkboxStateClass = {
+    disabled: 'bg-bg-disabled cursor-not-allowed',
+    checked:
+      'bg-bg-primaryInteraction border-none hover:bg-bg-primaryHover active:bg-bg-primaryPressed',
+    unchecked: 'bg-bg-primary border-border-tertiaryInteraction',
+  };
+
+  const currentCheckboxClass = disabled
+    ? checkboxStateClass.disabled
+    : checked
+      ? checkboxStateClass.checked
+      : checkboxStateClass.unchecked;
+
+  const labelBaseClass = 'font-style-subHeading';
+  const labelStateClass = disabled
+    ? 'text-text-disabled'
+    : 'text-text-secondary';
+
+  const iconColors = {
+    disabled: 'var(--color-icon-tertiary)',
+    checked: 'var(--color-icon-inverse)',
+    unchecked: 'var(--color-icon-tertiary)',
+  };
+
+  const getIconColor = () => {
+    if (disabled) return iconColors.disabled;
+    if (checked) return iconColors.checked;
+    return iconColors.unchecked;
+  };
+
+  const handleClick = () => {
+    if (!disabled && onChange) {
+      onChange(!checked);
+    }
+  };
+
+  return (
+    <div
+      className={cn(
+        'gap-sm flex items-center',
+        disabled ? 'cursor-not-allowed' : 'cursor-pointer',
+      )}
+      onClick={handleClick}
+    >
+      <div className={cn(checkboxBaseClass, currentCheckboxClass)}>
+        <Icon
+          iconName="check"
+          iconProps={{ width: 16, height: 16 }}
+          color={getIconColor()}
+        />
+      </div>
+      {label && (
+        <span className={cn(labelBaseClass, labelStateClass, className)}>
+          {label}
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default Checkbox;

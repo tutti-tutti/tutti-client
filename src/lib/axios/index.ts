128758bc8e4b40a9f723c92476ada2d6b0b5f119
vgotu99 <vgotu99@gmail.com>
2025-03-19 16:24:42 +0900

    feat: axios interceptors로 서버요청일 경우에만 header에 accessToken을 추가하고 silence refresh 하도록 구현 (#107)

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -5,6 +5,7 @@ import {
   SERVER_API_BASE_URL,
   SERVER_API_VERSION_V1,
 } from '@/constants';
+import { getAccessToken, refreshAccessToken, removeTokens } from '@/services';
 
 const isServer = typeof window === 'undefined';
 
@@ -19,3 +20,40 @@ export const axiosInstance = axios.create({
   },
   timeout: 10000,
 });
+
+if (isServer) {
+  axiosInstance.interceptors.request.use(
+    async config => {
+      const accessToken = await getAccessToken();
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+
+      return config;
+    },
+
+    error => Promise.reject(error),
+  );
+
+  axiosInstance.interceptors.response.use(
+    response => response,
+    async error => {
+      if (error.response?.status === 401 && !error.config._retry) {
+        error.config._retry = true;
+
+        try {
+          const newAccessToken = await refreshAccessToken();
+
+          error.config.headers.Authorization = `Bearer ${newAccessToken}`;
+
+          return axiosInstance(error.config);
+        } catch (refreshError) {
+          removeTokens();
+          return Promise.reject(refreshError);
+        }
+      }
+
+      return Promise.reject(error);
+    },
+  );
+}

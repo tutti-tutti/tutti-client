f924cac3f8c2a41863d44adf90d3678b523b92f6
Boongranii <jobc4550@gmail.com>
2025-03-19 10:24:57 +0900

    [✨ feat] 체크박스 컴포넌트 제작 (#110) (#113)
    * feat: 체크박스 컴포넌트 제작 및 스토리북 작성 (#110)
    
    * feat: 배럴 export 추가 (#110)
    
    * refactor: checkbox 컴포넌트 접근성 개선 (#110)
    
    * feat: checkbox 컴포넌트의 접근성을 높이기 위한 Input 요소의 프로퍼티 추가 (#110)

---
+++ src/stories/Checkbox.stories.ts
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Checkbox } from '@/components';
+
+const meta: Meta<typeof Checkbox> = {
+  title: 'Checkbox/Checkbox',
+  component: Checkbox,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    label: {
+      control: 'text',
+    },
+    checked: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    onChange: {
+      action: 'changed',
+    },
+    className: {
+      control: 'text',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Checkbox>;
+
+export const Default: Story = {
+  args: {
+    label: '기본 체크박스',
+  },
+};
+
+export const Checked: Story = {
+  args: {
+    label: '체크된 체크박스',
+    checked: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: '비활성화된 체크박스',
+    disabled: true,
+  },
+};
+
+export const WithoutLabel: Story = {
+  args: {
+    checked: false,
+  },
+};

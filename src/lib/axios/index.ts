52e2723faaccf1c4cab809f5106a3668de2191e0
vgotu99 <vgotu99@gmail.com>
2025-03-20 19:03:13 +0900

    hotfix: 클라이언트 컴포넌트에서 참조해야할 환경변수에 NEXT_PUBLIC 접두사 추가 (#122)

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -1,17 +1,16 @@
 import axios from 'axios';
 
 import {
-  API_ROUTE_BASE_URL,
-  SERVER_API_BASE_URL,
-  SERVER_API_VERSION_V1,
+  NEXT_PUBLIC_API_ROUTE_BASE_URL,
+  SERVER_API_V1_BASE_URL,
 } from '@/constants';
 import { getAccessToken, renewAccessToken, removeTokens } from '@/services';
 
 const isServer = typeof window === 'undefined';
 
 const baseURL = isServer
-  ? `${SERVER_API_BASE_URL}${SERVER_API_VERSION_V1}`
-  : API_ROUTE_BASE_URL;
+  ? SERVER_API_V1_BASE_URL
+  : NEXT_PUBLIC_API_ROUTE_BASE_URL;
 
 export const axiosInstance = axios.create({
   baseURL,

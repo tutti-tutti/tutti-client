82844e7329620a7ca51747796a0c0dd16b0c7183
gold <goldegg127@gmail.com>
2025-03-21 03:53:55 +0900

    fix: 서버 컴포넌트에서 스켈레톤 UI 노출되도록 직접 api 호출로 수정 (#93)

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,14 +1,20 @@
+import { Suspense } from 'react';
+
 import {
   ProductList,
   RecommendProductList,
   ProductListWrapTemp,
+  ProductListSkeleton,
 } from '@/components';
 
 const HomePage = () => {
   return (
     <>
       <RecommendProductList categoryName="식료품" />
-      <ProductList />
+
+      <Suspense fallback={<ProductListSkeleton />}>
+        <ProductList />
+      </Suspense>
 
       {/**TODO - 삭제 예정: 클라이언트 컴포넌트를 포함하는 서버 컴포넌트 사용 예시 용 */}
       <ProductListWrapTemp />
--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -1,33 +1,20 @@
-import { Suspense } from 'react';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
-import { getQueryClient } from "@/lib/tanstack";
-import { getProductsQueryOptions } from '@/queries';
-import type { Product, ProductListAPISchema } from '@/types';
-import { ProductListSkeleton} from '@/components';
+import { fetchProducts } from '@/services';
+import type { Product } from '@/types';
 import ProductItem from './ProductItem';
 
 const ProductList = async () => {
-  const queryClient = getQueryClient();
-  
-  await queryClient.prefetchQuery(getProductsQueryOptions);
-
-  const dehydratedState = dehydrate(queryClient);
-  const data = dehydratedState.queries[0].state.data as ProductListAPISchema;
+  const data = await fetchProducts();
   const productItems = data[0].latestList || [];
 
   return (
-    <Suspense fallback={<ProductListSkeleton />}>
-      <HydrationBoundary state={dehydratedState}>
-        <section>
-          <ul className="gap-x-md gap-y-7xl py-7xl grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {productItems.map((item: Product) => (
-              <ProductItem key={item.productId} {...item} />
-            ))}
-          </ul>
-        </section>
-        </HydrationBoundary>
-    </Suspense>
+    <section>
+      <ul className="gap-x-md gap-y-7xl py-7xl grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {productItems.map((item: Product) => (
+          <ProductItem key={item.productId} {...item} />
+        ))}
+      </ul>
+  </section>
   );
 };
 

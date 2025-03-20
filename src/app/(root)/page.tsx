06148131b049ef0d09a25486473aa3af803b28f1
gold <goldegg127@gmail.com>
2025-03-21 00:33:45 +0900

    feat: 코드 일관성을 위한 서버컴포넌트에서 쿼리 호출하여 데이터 fetch 예시 (#93)

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,8 +1,5 @@
-import { Suspense } from 'react';
-
 import {
   ProductList,
-  ProductListSkeleton,
   RecommendProductList,
   ProductListWrapTemp,
 } from '@/components';
@@ -11,11 +8,9 @@ const HomePage = () => {
   return (
     <>
       <RecommendProductList categoryName="식료품" />
-      <Suspense fallback={<ProductListSkeleton />}>
-        <ProductList />
-      </Suspense>
+      <ProductList />
 
-      {/**TODO - 클라이언트 컴포넌트를 포함하는 서버 컴포넌트 사용 예시 용 */}
+      {/**TODO - 삭제 예정: 클라이언트 컴포넌트를 포함하는 서버 컴포넌트 사용 예시 용 */}
       <ProductListWrapTemp />
     </>
   );

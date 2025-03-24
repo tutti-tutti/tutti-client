f3a484a1a1a9838a234fe3013de34f6785905905
bbjbc <jobc4550@gmail.com>
2025-03-24 12:43:44 +0900

    feat: 장바구니와 상품 상세에 대한 api 라우트 추가 (#141)

---
+++ src/app/api/cart/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+
+import { addCart } from '@/services';
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+
+    if (!body.productItemId || !body.quantity) {
+      return NextResponse.json(
+        { message: 'productItemId와 quantity가 필요합니다.' },
+        { status: 400 },
+      );
+    }
+
+    const products = await addCart({
+      productItemId: body.productItemId,
+      quantity: body.quantity,
+    });
+
+    return NextResponse.json(products);
+  } catch (error) {
+    return NextResponse.json(
+      { message: (error as Error).message },
+      { status: 500 },
+    );
+  }
+};

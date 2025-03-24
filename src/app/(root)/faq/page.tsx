b6907ef248a09a04166c2b98b6f0de1695db4099
vgotu99 <vgotu99@gmail.com>
2025-03-24 16:54:53 +0900

    refactor: 정적 페이지가 아닌 ssr로 동작하도록 다시 롤백 (#117)

--- src/app/(root)/faq/page.tsx
+++ src/app/(root)/faq/page.tsx
@@ -6,7 +6,6 @@ import {
   SubCategoryFilter,
 } from '@/components';
 import { faqSearchAction } from '@/server-actions';
-import { fetchMainCategories, fetchSubCategories } from '@/services';
 
 type SearchParamsProps = {
   searchParams: {
@@ -17,38 +16,13 @@ type SearchParamsProps = {
   };
 };
 
-export const dynamic = 'force-static';
-export const revalidate = 3600;
-
-export const generateStaticParams = async () => {
-  const categories = await fetchMainCategories();
-
-  const paths = [{}];
-
-  const categoryPaths = await Promise.all(
-    categories.map(async (category: string) => {
-      const categoryPath = { category };
-
-      const subCategories = await fetchSubCategories(category);
-
-      const subCategoryPaths = subCategories.map((subCategory: string) => ({
-        category,
-        sub: subCategory,
-      }));
-
-      return [categoryPath, ...subCategoryPaths];
-    }),
-  );
-
-  return [...paths, ...categoryPaths.flat()];
-};
 const FaqPage = async ({ searchParams }: SearchParamsProps) => {
   const {
     category: categorySearchParams,
     sub: subSearchParams,
     faq: faqSearchParams,
     query: querySearchParams,
-  } = searchParams;
+  } = await searchParams;
 
   return (
     <div className="md:px-7xl gap-lg mb-7xl flex flex-col items-center">

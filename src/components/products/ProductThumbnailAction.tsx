11af450e98daa3a59d41d405d06031f43628fb16
bbjbc <jobc4550@gmail.com>
2025-03-24 12:50:46 +0900

    refactor: 상품 썸네일 액션에 addCart 서비스 함수 추가 (#141)

--- src/components/products/ProductThumbnailAction.tsx
+++ src/components/products/ProductThumbnailAction.tsx
@@ -1,18 +1,27 @@
 'use client';
 
+import { addCart } from '@/services';
 import { IconButton } from '../common';
 
-const ProductThumbnailAction = () => {
-  const handleCartClick = () => {
-    console.log('장바구니에 담기'); // 추후 구현
+const ProductThumbnailAction = ({ productId }: { productId: number }) => {
+  const handleCartClick = async () => {
+    await addCart({ productItemId: productId, quantity: 1 });
   };
 
   const handleHeartClick = () => {
     console.log('이 상품을 좋아해요'); // 추후 구현
   };
 
+  const handleContainerClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
   return (
-    <div className="gap-sm absolute right-4 bottom-4 flex items-center">
+    <div
+      className="gap-sm absolute right-4 bottom-4 flex items-center"
+      onClick={handleContainerClick}
+    >
       <IconButton
         icon="cart"
         variant="primaryShadow"
--- src/components/products/product-detail/ProductActions.tsx
+++ src/components/products/product-detail/ProductActions.tsx
@@ -1,11 +1,17 @@
 import { Button } from '@/components';
+import { addCart } from '@/services';
 
-const ProductActions = () => {
+const ProductActions = ({ productId }: { productId: number }) => {
+  const handleCartClick = async () => {
+    await addCart({ productItemId: productId, quantity: 1 });
+  };
+
   return (
     <div className="gap-sm grid grid-cols-2">
       <Button
         variant="secondaryOutline"
         className="font-style-subHeading flex-auto"
+        onClick={handleCartClick}
       >
         장바구니 담기
       </Button>

671ebf87222dd87aa9342eeaf2c68d2d50ac0530
vgotu99 <vgotu99@gmail.com>
2025-03-26 03:33:07 +0900

    feat: 리뷰 등록을 위한 서비스 함수 구현 (#144)

---
+++ src/services/reviewService.ts
@@ -0,0 +1,24 @@
+import { REVIEW_ENDPOINTS } from '@/constants';
+import { axiosInstance } from '@/lib';
+
+export const createReview = async (
+  orderId: string,
+  productItemId: string,
+  rating: string,
+  content: string,
+  nickname: string,
+) => {
+  const numberOrderId = Number(orderId);
+  const numberProductItemId = Number(productItemId);
+  const numberRating = Number(rating);
+
+  const { data } = await axiosInstance.post(REVIEW_ENDPOINTS.CREATE_REVIEW, {
+    orderId: numberOrderId,
+    productItemId: numberProductItemId,
+    numberRating,
+    content,
+    nickname,
+  });
+
+  return data;
+};

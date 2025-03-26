2b62b516167af93a8959ccb37dd946cccef38b86
bbjbc <jobc4550@gmail.com>
2025-03-26 17:06:00 +0900

    feat: 연동을 위한 msw 장바구니 핸들러 주석 처리 (#157)

--- src/lib/msw/handlers/cartHandlers.ts
+++ src/lib/msw/handlers/cartHandlers.ts
@@ -1,11 +1,11 @@
-import { http, HttpResponse } from 'msw';
+// import { http, HttpResponse } from 'msw';
 
-import { getMswEndpoint } from '@/utils';
-import { cart } from '@/mocks';
-import { CART_ENDPOINTS } from '@/constants';
+// import { getMswEndpoint } from '@/utils';
+// import { cart } from '@/mocks';
+// import { CART_ENDPOINTS } from '@/constants';
 
 export const cartHandlers = [
-  http.get(getMswEndpoint(CART_ENDPOINTS.LIST), () => {
-    return HttpResponse.json(cart);
-  }),
+  // http.get(getMswEndpoint(CART_ENDPOINTS.LIST), () => {
+  //   return HttpResponse.json(cart);
+  // }),
 ];

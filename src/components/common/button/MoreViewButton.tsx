fdc2bda90ea1e4a0e5904f139a823f81c9920bf9
bbjbc <jobc4550@gmail.com>
2025-03-26 20:46:05 +0900

    feat: 더보기 버튼에 onClick 프로퍼티 추가 (#160)

--- src/components/common/button/MoreViewButton.tsx
+++ src/components/common/button/MoreViewButton.tsx
@@ -1,12 +1,17 @@
 import Button from './Button';
 
-const MoreViewButton = () => {
+interface MoreViewButtonProps {
+  onClick?: () => void;
+}
+
+const MoreViewButton = ({ onClick }: MoreViewButtonProps) => {
   return (
     <Button
       className="!py-lg text-text-secondary w-full text-xl"
       variant="transparent"
       icon="chevronsDown"
       iconPosition="right"
+      onClick={onClick}
     >
       더보기
     </Button>

426ee506504867a15b167fba2f17ae7bfaac2953
bbjbc <jobc4550@gmail.com>
2025-03-26 17:05:00 +0900

    refactor: 장바구니 페이지에 추천 상품 보여주기 임시 폐쇄 (#157)

--- src/app/(root)/cart/page.tsx
+++ src/app/(root)/cart/page.tsx
@@ -1,20 +1,12 @@
-import { HydrationBoundary } from '@tanstack/react-query';
-
-import { CartInfo, RecommendProductList } from '@/components';
-import { getDehydratedState } from '@/utils';
-import { cartQueryOptions } from '@/queries';
+import { CartInfo } from '@/components';
 
 const CartPage = () => {
-  const dehydratedState = getDehydratedState(cartQueryOptions);
-
   return (
     <div className="gap-3xl flex flex-col">
-      <HydrationBoundary state={dehydratedState}>
-        <CartInfo />
-      </HydrationBoundary>
-      <div className="mb-7xl">
+      <CartInfo />
+      {/* <div className="mb-7xl">
         <RecommendProductList categoryName="식료품" />
-      </div>
+      </div> */}
     </div>
   );
 };

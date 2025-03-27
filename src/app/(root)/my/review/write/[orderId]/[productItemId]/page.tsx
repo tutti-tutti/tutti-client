956ad2de753c284a06c5e14c367f3dcd44a157a3
vgotu99 <vgotu99@gmail.com>
2025-03-28 00:07:54 +0900

    design: 하단 푸터와의 여백 추가 (#144)

--- src/app/(root)/my/review/write/[orderId]/[productItemId]/page.tsx
+++ src/app/(root)/my/review/write/[orderId]/[productItemId]/page.tsx
@@ -11,7 +11,7 @@ const CreateReviewPage = async ({ params }: CreateReviewPageParams) => {
   const { orderId, productItemId } = await params;
 
   return (
-    <div className="flex h-dvh justify-center">
+    <div className="mb-7xl flex justify-center">
       <div className="flex w-3xl flex-col items-center">
         <h1 className="font-style-title mb-5xl">리뷰 작성</h1>
         <ReviewProductInfo orderId={orderId} productItemId={productItemId} />

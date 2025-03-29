ad5103e2678d0d68468d4ce74986c11e9bcbf794
bbjbc <jobc4550@gmail.com>
2025-03-29 18:01:19 +0900

    refactor: 상품 카테고리 클릭 시 사용자가 인지할 수 있도록 색상 변경 (#181)

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,24 +3,19 @@ import { Suspense } from 'react';
 import {
   ProductList,
   RecommendProductList,
-  ProductListSkeleton,
   ProductCategory,
+  CategorySkeleton,
 } from '@/components';
 
 const HomePage = () => {
   return (
     <>
-      <div className="gap-xl flex flex-col">
-        <h3 className="text-text-primary font-style-heading text-center">
-          지혜 쇼핑몰 카테고리
-        </h3>
-        <ProductCategory />
-      </div>
+      <Suspense fallback={<CategorySkeleton />}>
+        <ProductCategory isMainPage />
+      </Suspense>
       <RecommendProductList categoryName="식료품" />
 
-      <Suspense fallback={<ProductListSkeleton />}>
-        <ProductList />
-      </Suspense>
+      <ProductList />
     </>
   );
 };

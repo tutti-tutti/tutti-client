2e0780adae7b0392820755237f9041b2b6f96d4d
bbjbc <jobc4550@gmail.com>
2025-03-29 17:58:14 +0900

    feat: 서버 DB 이미지 도메인 추가 (#181)

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,31 @@ const nextConfig: NextConfig = {
         hostname: 'shopping-phinf.pstatic.net',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'thumbnail10.coupangcdn.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'content.foodspring.co.kr',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sui.ssgcdn.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'www.efoodzone.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.homeplus.kr',
+        pathname: '/**',
+      },
     ],
   },
 };

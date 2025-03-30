7caa1942b67e1dc4c96457c93d7f0d8cfdd29fe4
vgotu99 <vgotu99@gmail.com>
2025-03-30 20:47:05 +0900

    feat: 감성분석 뱃지 호버 시 감성분석 피드백을 위한 텍스트 렌더링 (#190)

--- src/components/reviews/product-review/ReviewBadge.tsx
+++ src/components/reviews/product-review/ReviewBadge.tsx
@@ -1,4 +1,6 @@
-import type { ReactNode } from 'react';
+'use client';
+
+import { useState, type ReactNode } from 'react';
 
 import { Icon } from '@/components';
 import { cn } from '@/utils';
@@ -10,11 +12,12 @@ interface ReviewBadgeProps {
 }
 
 const ReviewBadge = ({ type, children, textSize }: ReviewBadgeProps) => {
+  const [hover, setHover] = useState(false);
+
   const defaultClass = {
-    bg: 'gap-xs px-sm py-xs items-center rounded-xl inline-flex',
+    bg: 'gap-xs px-sm py-xs items-center rounded-xl inline-flex border border-transparent',
     text: `font-style-${textSize}`,
   };
-
   const typeClass =
     type === 'positive'
       ? {
@@ -27,11 +30,30 @@ const ReviewBadge = ({ type, children, textSize }: ReviewBadgeProps) => {
           icon: 'text-icon-danger',
           text: 'text-text-dangerBold',
         };
+  const hoverClass = {
+    bg: 'box-border border hover:border-border-primaryHover hover:text-text-primaryHover active:border-border-primaryPressed active:text-text-primaryPressed cursor-pointer',
+    icon: 'hover:border-border-primaryHover hover:text-text-primaryHover active:border-border-primaryPressed active:text-text-primaryPressed',
+    text: 'hover:border-border-primaryHover hover:text-text-primaryHover active:border-border-primaryPressed active:text-text-primaryPressed',
+  };
+  const badgeClass = hover && textSize === 'paragraph' ? hoverClass : typeClass;
+  const hoverText =
+    type === 'positive'
+      ? '긍정적인 반응의 리뷰가 아니에요'
+      : '부정적인 반응의 리뷰가 아니에요';
+  const badgeText = hover && textSize === 'paragraph' ? hoverText : children;
+
+  const handleHover = () => {
+    setHover(prev => !prev);
+  };
 
   return (
-    <div className={cn(defaultClass.bg, typeClass.bg)}>
-      <Icon iconName={type} className={typeClass.icon} />
-      <div className={cn(defaultClass.text, typeClass.text)}>{children}</div>
+    <div
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+      className={cn(defaultClass.bg, badgeClass.bg)}
+    >
+      <Icon iconName={type} className={badgeClass.icon} />
+      <div className={cn(defaultClass.text, badgeClass.text)}>{badgeText}</div>
     </div>
   );
 };

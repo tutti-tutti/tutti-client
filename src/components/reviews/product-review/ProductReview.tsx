dadc4f580f5bfdeb4f16ecefa7b80b46c768dffa
vgotu99 <vgotu99@gmail.com>
2025-03-31 19:40:48 +0900

    feat: prefetchInfiniteQuery로 서버사이드에서 상품별 리뷰 초기 데이터 프리페칭 (#193)

--- src/components/reviews/product-review/ProductReview.tsx
+++ src/components/reviews/product-review/ProductReview.tsx
@@ -1,11 +1,40 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+
+import { fetchReviews } from '@/services';
+import { reviewServerStore } from '@/stores';
+import { QUERY_KEYS_ENDPOINT } from '@/constants';
 import ReviewList from './ReviewList';
 import AverageReview from './AverageReview';
 
 const ProductReview = async () => {
+  const { getParams } = reviewServerStore();
+  const { productIdParams, reviewSortSearchParams } = getParams();
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: [
+      QUERY_KEYS_ENDPOINT.REVIEWS,
+      productIdParams,
+      reviewSortSearchParams,
+    ],
+    queryFn: () => fetchReviews(productIdParams, reviewSortSearchParams, 10),
+    initialPageParam: null,
+  });
+
   return (
     <div>
       <AverageReview />
-      <ReviewList />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ReviewList
+          productIdParams={productIdParams}
+          reviewSortSearchParams={reviewSortSearchParams}
+        />
+      </HydrationBoundary>
     </div>
   );
 };
--- src/types/reviewType.ts
+++ src/types/reviewType.ts
@@ -4,3 +4,28 @@ export interface CreateReviewState {
   reviewContentError?: string;
   error?: string;
 }
+
+export interface ReviewPageParam {
+  reviewId?: number;
+  extraData?: number;
+}
+
+export interface ReviewItemAPISchema {
+  id: number;
+  productItemId: number;
+  nickname: string;
+  content: string;
+  rating: number;
+  reviewImageUrls: string[];
+  likeCount: number;
+  sentiment: 'positive' | 'negative';
+  sentimentProbability: number;
+  createdAt: string;
+  liked?: boolean;
+}
+
+export interface ReviewsResponse {
+  reviews: ReviewItemAPISchema[];
+  cursor: ReviewPageParam;
+  hasNext: boolean;
+}

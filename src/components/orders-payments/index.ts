a4b5baca4f22c29da6ca8859e36ab70efdee39dd
gold <goldegg127@gmail.com>
2025-04-01 18:11:04 +0900

    feat: 주문 상세 페이지 생성 (#203)

--- src/components/orders-payments/index.ts
+++ src/components/orders-payments/index.ts
@@ -7,3 +7,4 @@ export * from './OrderHistoryList';
 export { default as ProductName } from './ProductName';
 export { default as ProductAmoutInfo } from './ProductAmoutInfo';
 export { default as ProductOptionInfo } from './ProductOptionInfo';
+export { default as OrderTableInfoSection } from './OrderTableInfoSection';

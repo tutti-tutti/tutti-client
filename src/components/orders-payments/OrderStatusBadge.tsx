6253747ee0b73d8a55c08a919f78a3a4f3b60c01
gold <goldegg127@gmail.com>
2025-04-01 22:43:03 +0900

    feat: 주문상태 뱃지 컴포넌트 생성 - OrderStatusBadge (#203)

---
+++ src/components/orders-payments/OrderStatusBadge.tsx
@@ -0,0 +1,36 @@
+import type { BadgeVariant } from '@/types';
+import { Badge } from '@/components';
+
+type OrderStatus = 'CANCELED' | 'DONE' | string;
+
+interface OrderStatusBadgeProps {
+  orderStatus: OrderStatus;
+}
+
+interface StatusInfo {
+  label: string;
+  variant: BadgeVariant;
+}
+
+const OrderStatusBadge = ({ orderStatus }: OrderStatusBadgeProps) => {
+  console.log('orderStatus ', orderStatus);
+
+  const status: Record<OrderStatus, StatusInfo> = {
+    CANCELED: {
+      label: '주문 취소',
+      variant: 'secondaryOutlineSquare',
+    },
+    DONE: {
+      label: '결제 완료',
+      variant: 'successOutlineSquare',
+    },
+  };
+
+  return (
+    <Badge variant={status[orderStatus]?.variant || 'primaryOutlineSquare'}>
+      {status[orderStatus]?.label || '확인 중'}
+    </Badge>
+  );
+};
+
+export default OrderStatusBadge;
--- src/components/orders-payments/index.ts
+++ src/components/orders-payments/index.ts
@@ -8,3 +8,4 @@ export { default as ProductName } from './ProductName';
 export { default as ProductAmoutInfo } from './ProductAmoutInfo';
 export { default as ProductOptionInfo } from './ProductOptionInfo';
 export { default as OrderTableInfoSection } from './OrderTableInfoSection';
+export { default as OrderStatusBadge } from './OrderStatusBadge';

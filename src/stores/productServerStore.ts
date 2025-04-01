e978bd9f1f84df0900fe2ed740b9d0cb6cafb07a
bbjbc <jobc4550@gmail.com>
2025-04-01 23:16:33 +0900

    feat: 상품 페이지네이션 서버 캐시 스토어 생성 (#224)

---
+++ src/stores/productServerStore.ts
@@ -0,0 +1,20 @@
+import { cache } from 'react';
+
+interface ProductParams {
+  cursorId?: number;
+  size: number;
+}
+
+export const productServerStore = cache(() => {
+  const params: ProductParams = {
+    cursorId: undefined,
+    size: 30,
+  };
+
+  return {
+    getParams: () => params,
+    setParams: (newParams: Partial<ProductParams>) => {
+      Object.assign(params, newParams);
+    },
+  };
+});

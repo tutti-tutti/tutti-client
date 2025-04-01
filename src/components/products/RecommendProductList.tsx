41bfa2f8ab70ecf7336f379c2cc6b7b81eb376c8
Boongranii <jobc4550@gmail.com>
2025-04-01 14:28:14 +0900

    refactor: 메인페이지에서의 추천 상품 메시지 조건부 렌더링 (#216) (#218)

--- src/components/products/RecommendProductList.tsx
+++ src/components/products/RecommendProductList.tsx
@@ -6,20 +6,29 @@ import { Icon } from '../common';
 const RecommendProductList = async ({
   categoryName,
 }: {
-  categoryName: string;
+  categoryName?: string;
 }) => {
   const recommendedProducts = await fetchRecommededProducts();
-  const titleContent = PRODUCTS_CONSTANTS.getRecommendListTitle(categoryName);
+  const titleContent =
+    categoryName && PRODUCTS_CONSTANTS.getRecommendListTitle(categoryName);
 
   return (
     <div className="gap-sm md:gap-3xl py-lg md:py-xl border-border-secondary flex flex-col border-y">
       <div className="gap-sm flex items-center justify-center">
         <Icon iconName="tinyLogo" />
         <h2 className="font-style-subHeading text-brand-gradient text-center">
-          <span className="whitespace-pre-line md:hidden">
-            {titleContent.mobile}
-          </span>
-          <span className="hidden md:inline">{titleContent.desktop}</span>
+          {titleContent ? (
+            <>
+              <span className="whitespace-pre-line md:hidden">
+                {titleContent.mobile}
+              </span>
+              <span className="hidden md:inline">{titleContent.desktop}</span>
+            </>
+          ) : (
+            <span>
+              고객님의 데이터가 아직 없어 좋아요순으로 상품을 추천한 리스트에요!
+            </span>
+          )}
         </h2>
       </div>
       <RecommendCarousel recommendedProducts={recommendedProducts} />

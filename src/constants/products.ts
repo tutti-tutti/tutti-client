1b88fce0d5020cad814ca8695b7b3138b186915b
bbjbc <jobc4550@gmail.com>
2025-04-07 16:11:59 +0900

    refactor: 장바구니 관련 상수 모듈화 (#273)

--- src/constants/products.ts
+++ src/constants/products.ts
@@ -3,6 +3,8 @@ export const PRODUCTS_CONSTANTS = {
     desktop: `당신의 쇼핑 데이터를 분석한 맞춤 ${categoryName} 리스트입니다.`,
     mobile: `당신의 쇼핑 데이터를 분석한\n맞춤 ${categoryName} 리스트입니다.`,
   }),
+  MORE_VIEW: '더보기',
+  FETCH_FAIL_MESSAGE: '상품 정보를 불러오는 데 실패했습니다.',
 };
 
 export const PRODUCT_LIST_LAYOUT_CONFIG = {

206421223f6a568d2cb3d7966b91ad68ce00c3e4
vgotu99 <vgotu99@gmail.com>
2025-04-08 02:02:07 +0900

    refactor: 각 페이지에서 사용하는 Form 컴포넌트가 아닌 재사용성을 높인 AuthForm 컴포넌트를 사용하도록 수정 (#271)

--- src/app/(auth)/reset-password/page.tsx
+++ src/app/(auth)/reset-password/page.tsx
@@ -1,7 +1,14 @@
-import { ResetPwForm } from '@/components';
+import { AuthForm } from '@/components';
+import { AUTH_CONSTANTS } from '@/constants';
 
 const ResetPasswordPage = () => {
-  return <ResetPwForm />;
+  return (
+    <AuthForm
+      type="resetPw"
+      title={AUTH_CONSTANTS.RESET_PW}
+      redirectPath="/signin"
+    />
+  );
 };
 
 export default ResetPasswordPage;

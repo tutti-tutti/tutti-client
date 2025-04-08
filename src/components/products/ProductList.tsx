6d1d40cc36a1dec4879c56cad50dd8e151f58fa1
Boongranii <jobc4550@gmail.com>
2025-04-08 17:22:42 +0900

    [✨ feat] 상품 리스트에서도 리뷰 별점과 개수 볼 수 있도록 변경 (#276)  (#278)
    * refactor: 상품 페이지네이션에서 프리페치 시 20개 데이터 가져오도록 변경 (#276)
    
    * refactor: 상품 탐색 그리드가 4열로 변경되면서 desktop config 변경 (#276)
    
    * feat: 상품 리스트에 리뷰 별점 평균 및 개수 정보 추가 (#276)
    
    * feat: 별점 클릭 시 해당 상세 페이지의 리뷰 정보로 이동 (#276)
    
    * feat: 상품 리스트와 아이템 스토리북에 더미 리뷰 데이터 추가 (#276)
    
    * feat: 메인 레이아웃에 메타데이터 추가 (#276)
    
    * feat: 더보기 버튼을 한 번만 실행시키기 위한 세션스토리지에 키 값 저장 (#276)
    
    * design: 카테고리 상품 리스트 div 안으로 삽입 (#276)
    
    * feat: 상품 리뷰 정보를 가져오기 위한 서버액션 함수 제작 (#276)
    
    * refactor: 무한 스크롤로 데이터를 가져올 때마다 리뷰 정보 프리페칭하도록 수정 (#276)
    
    * refactor: 중복된 figure 태그 삭제 (#276)
    
    * refactor: og image 상대경로가 아닌 절대경로로 수정 (#276)
    
    * fix: 오탈자 수정 (#276)

--- src/components/products/ProductList.tsx
+++ src/components/products/ProductList.tsx
@@ -3,6 +3,9 @@ import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { productServerStore } from '@/stores';
 import { productsPrefetchInfiniteQueryOptions } from '@/queries';
 import { getQueryClient } from '@/lib';
+import { fetchProductReviewInfo } from '@/services';
+import { PRODUCTS_CONSTANTS } from '@/constants';
+import type { ProductReviewInfo } from '@/types';
 import ClientProductList from './ClientProductList';
 
 const ProductList = async () => {
@@ -11,13 +14,34 @@ const ProductList = async () => {
 
   const queryClient = getQueryClient();
 
-  await queryClient.prefetchInfiniteQuery(
+  const initialData = await queryClient.fetchInfiniteQuery(
     productsPrefetchInfiniteQueryOptions(cursorId, size),
+  );
+
+  const allProducts = initialData.pages.flatMap(page => page.content);
+  const productReviews: ProductReviewInfo[] = [];
+
+  await Promise.all(
+    allProducts.map(async product => {
+      try {
+        const reviewInfo = await fetchProductReviewInfo(
+          String(product.productId),
+        );
+        productReviews[product.productId] = reviewInfo;
+      } catch (error) {
+        console.error(PRODUCTS_CONSTANTS.FETCH_REVIEW_FAIL_MESSAGE, error);
+        productReviews[product.productId] = {
+          productId: product.productId,
+          avg: '0.0',
+          totalCount: 0,
+        };
+      }
+    }),
   );
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <ClientProductList />
+      <ClientProductList productReviews={productReviews} />
     </HydrationBoundary>
   );
 };

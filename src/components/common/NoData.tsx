67b3b51faaaa09f2fba76e917288223b0fc2794d
bbjbc <jobc4550@gmail.com>
2025-04-14 16:26:42 +0900

    design: no data 컴포넌트 스타일 수정 (#298)

--- src/components/common/NoData.tsx
+++ src/components/common/NoData.tsx
@@ -6,14 +6,14 @@ interface NoDataProps {
 
 const NoData = ({ children }: NoDataProps) => {
   return (
-    <div className="gap-lg md:gap-7xl py-5xl flex flex-col items-center justify-center md:flex-row">
+    <div className="gap-sm md:gap-md py-6xl flex flex-col items-center justify-center md:flex-row">
       <Icon
         iconName="notFound"
-        className="h-[228px] w-[228px] md:h-[346px] md:w-[346px]"
+        className="h-[32px] w-[32px] md:h-[40px] md:w-[40px]"
       />
 
       <div className="flex flex-col">
-        <div className="text-text-tertiaryInfo font-style-heading mb-sm md:mb-lg">
+        <div className="text-text-tertiaryInfo font-style-subHeading">
           {children}
         </div>
       </div>

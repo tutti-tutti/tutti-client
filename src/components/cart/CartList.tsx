a6741ff44280acab85635d9fd269929c8317c49d
bbjbc <jobc4550@gmail.com>
2025-04-14 17:44:44 +0900

    feat: 장바구니 리스트 pending 시에 스켈레톤이 리턴되도록 구현 (#298)

--- src/components/cart/CartList.tsx
+++ src/components/cart/CartList.tsx
@@ -3,20 +3,14 @@
 import { useCart } from '@/hooks';
 import { CART_CONSTANTS } from '@/constants';
 import CartItem from './CartItem';
+import CartItemSkeleton from './CartItemSkeleton';
 import { MoreViewButton, NoData } from '../common';
 
 const CartList = () => {
   const { items, isPending } = useCart();
 
   if (isPending) {
-    return (
-      <div className="py-xl text-center">
-        <p className="text-text-secondary font-style-heading">
-          {/* 추후 스켈레톤으로 구현 */}
-          {CART_CONSTANTS.LOADING_CART_MESSAGE}
-        </p>
-      </div>
-    );
+    return <CartItemSkeleton />;
   }
 
   if (items.length === 0) {

2c2394e0a069bcc5ae1a98f6a79c80cdb5769cde
gold <goldegg127@gmail.com>
2025-04-14 22:02:25 +0900

    feat: 주문내역 및 주문 상세에서 장바구니 추가 기능 추가 (#239)

--- src/components/orders-payments/OrderHistoryList/OrdersExtraActions.tsx
+++ src/components/orders-payments/OrderHistoryList/OrdersExtraActions.tsx
@@ -2,7 +2,17 @@
 
 import { redirect } from 'next/navigation';
 
-import { ROUTER_PATH, ORDER_CONSTANT, REVIEW_CONSTANTS } from '@/constants';
+import { useQueryClient } from '@tanstack/react-query';
+
+import {
+  ROUTER_PATH,
+  ORDER_CONSTANT,
+  REVIEW_CONSTANTS,
+  CART_CONSTANTS,
+} from '@/constants';
+import { toast } from '@/utils';
+import { addCart } from '@/services';
+import { cartQueryOptions } from '@/queries';
 import { ExtraButton } from '@/components';
 
 interface OrdersExtraActionsProps {
@@ -13,6 +23,8 @@ interface OrdersExtraActionsProps {
   isCanceled: boolean;
 }
 
+const { ADD_SUCCESS_MESSAGE, ADD_FAIL_MESSAGE } = CART_CONSTANTS;
+
 const OrdersExtraActions = ({
   orderId,
   orderSheetNo,
@@ -20,8 +32,32 @@ const OrdersExtraActions = ({
   productItemId,
   isCanceled,
 }: OrdersExtraActionsProps) => {
+  const queryClient = useQueryClient();
+
   const handleWriteReview = () =>
     redirect(ROUTER_PATH.REVIEW_PRODUCT(orderId, productId, productItemId));
+
+  const handleAddCart = async () => {
+    try {
+      const cartItems = [{ productItemId, quantity: 1 }];
+
+      const result = await addCart(productId, cartItems);
+
+      if (result.isSuccess) {
+        toast.success(result.message || ADD_SUCCESS_MESSAGE);
+
+        await queryClient.invalidateQueries({
+          queryKey: cartQueryOptions.queryKey,
+        });
+        await queryClient.prefetchQuery(cartQueryOptions);
+      } else {
+        toast.error(result.message || ADD_FAIL_MESSAGE);
+      }
+    } catch (error) {
+      console.error(ADD_FAIL_MESSAGE, error);
+      toast.error(ADD_FAIL_MESSAGE);
+    }
+  };
 
   return (
     <article className="w-full md:flex md:justify-between">
@@ -29,13 +65,17 @@ const OrdersExtraActions = ({
         {ORDER_CONSTANT.ORDER_SHEET_NO} : {orderSheetNo}
       </div>
 
-      {!isCanceled && (
-        <div className="gap-xs flex justify-end">
+      <div className="gap-xs flex justify-end">
+        <ExtraButton onClick={handleAddCart}>
+          {CART_CONSTANTS.ADD_CART}
+        </ExtraButton>
+
+        {!isCanceled && (
           <ExtraButton onClick={handleWriteReview}>
             {REVIEW_CONSTANTS.CREATE.LABEL}
           </ExtraButton>
-        </div>
-      )}
+        )}
+      </div>
     </article>
   );
 };

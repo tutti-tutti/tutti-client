98fde8cc9cdac188584825bd19cbe6c47b158508
gold <goldegg127@gmail.com>
2025-04-17 11:59:30 +0900

    refactor: paths 상수에서 기존 레거시 코드 호환하여 관리되도록 수정 (#307)

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,43 +1,3 @@
-export const ROUTER_PATH = {
-  HOME: '/',
-  SIGNUP: '/signup',
-  LOGIN: '/signin',
-  RESET_PW: '/reset-password',
-  FAQS: '/faqs',
-  CART: '/cart',
-  MY: '/my',
-  MY_PAGE: '/my/orders',
-  COUNTRY_SETTING: '/country-setting',
-  CHECKOUT: (checkoutRequestItems: string) =>
-    `/checkout?checkoutRequestItems=${checkoutRequestItems}`,
-  CHECKOUT_SUCCESS: '/checkout/success',
-  CHECKOUT_FAIL: '/checkout/fail',
-  ORDERS_HISTORY: '/my/orders',
-  ORDERS_DETAIL: (orderId: number) => `/my/orders/${orderId}`,
-  PRODUCT_DETAIL: (productId: number) => `/products/${productId}`,
-  PRODUCT_CATEGORY: (categoryId: string) => `/products?category=${categoryId}`,
-  REVIEW_PRODUCT: (orderId: number, productId: number, productItemId: number) =>
-    `/my/review/write/${orderId}/${productId}/${productItemId}`,
-  ERROR_AUTHORIZED: '/error/authorized', // name 없음
-  ERROR_UNAUTHORIZED: '/error/unauthorized', // name 없음
-  ERROR_RESTRICTED: '/error/restricted', // name 없음
-};
-
-export const PATH_NAME = {
-  HOME: '홈',
-  SIGNUP: '회원가입',
-  LOGIN: '로그인',
-  LOGOUT: '로그아웃', // url 없음
-  FAQS: '자주 묻는 질문',
-  CART: '장바구니',
-  ORDER_CHECKOUT: '주문 결제',
-  ORDER_HISTORY: '주문 내역',
-  ORDER_DETAIL: '주문 상세',
-  MY_PAGE: '마이페이지',
-  CURRENT_COUNTRY: '현재 국가', // url 없음
-  COUNTRY_SETTING: '국가설정',
-};
-
 export const PATH = {
   HOME: {
     name: '홈',
@@ -80,9 +40,17 @@ export const PATH = {
     url: (checkoutRequestItems: string) =>
       `/checkout?checkoutRequestItems=${checkoutRequestItems}`,
   },
+  ORDER_CHECKOUT_SUCCESS: {
+    name: '주문 성공',
+    url: '/checkout/success',
+  },
+  ORDER_CHECKOUT_FAIL: {
+    name: '주문 실패',
+    url: '/checkout/fail',
+  },
   ORDER_HISTORY: {
-    name: '/my/orders',
-    url: '주문 내역',
+    name: '주문 내역',
+    url: '/my/orders',
   },
   ORDER_DETAIL: {
     name: '주문 상세',
@@ -103,41 +71,51 @@ export const PATH = {
   },
 };
 
-export const pageRouter = {
-  home: PATH.HOME.url,
-  countrySetting: PATH.COUNTRY_SETTING.url,
-  signup: PATH.SIGNUP.url,
-  signin: PATH.LOGIN.url,
-  resetPw: PATH.RESET_PW.url,
-  faqs: PATH.FAQS.url,
-  productCategory: (categoryId: string) =>
-    PATH.PRODUCT_CATEGORY.url(categoryId),
-  productDetail: (productId: number) => PATH.PRODUCT_DETAIL.url(productId),
-  cart: PATH.CART.url,
-  orderCheckout: (checkoutRequestItems: string) =>
-    PATH.ORDER_CHECKOUT.url(checkoutRequestItems),
-  orderHistory: () => PATH.ORDER_HISTORY.url,
-  orderDetail: (orderId: number) => PATH.ORDER_DETAIL.url(orderId),
-  my: PATH.MY.url,
-  myPage: PATH.MY_PAGE.url,
-  reviewProduct: (orderId: number, productId: number, productItemId: number) =>
-    PATH.REVIEW_PRODUCT.url(orderId, productId, productItemId),
-};
-
 export const PATH_ERROR = {
   ERROR_AUTHORIZED: '/error/authorized',
   ERROR_UNAUTHORIZED: '/error/unauthorized',
   ERROR_RESTRICTED: '/error/restricted',
 };
 
-export const HEADER_ROUTER = {
+/**TODO - 기존 레거시 코드 호완, 사용되는 코드 정리 시 제거 */
+export const PATH_NAME = {
   HOME: PATH.HOME.name,
   SIGNUP: PATH.SIGNUP.name,
   LOGIN: PATH.LOGIN.name,
   LOGOUT: '로그아웃',
   FAQS: PATH.FAQS.name,
   CART: PATH.CART.name,
+  ORDER_CHECKOUT: PATH.ORDER_CHECKOUT.name,
+  ORDER_HISTORY: PATH.ORDER_HISTORY.name,
+  ORDER_DETAIL: PATH.ORDER_DETAIL.name,
   MY_PAGE: PATH.MY_PAGE.name,
   CURRENT_COUNTRY: '현재 국가',
   COUNTRY_SETTING: PATH.COUNTRY_SETTING.name,
+};
+
+/**TODO - 기존 레거시 코드 호완, pageRouter 로 사용 시 제거 */
+export const ROUTER_PATH = {
+  HOME: PATH.HOME.url,
+  SIGNUP: PATH.SIGNUP.url,
+  LOGIN: PATH.LOGIN.url,
+  RESET_PW: PATH.RESET_PW.url,
+  FAQS: PATH.FAQS.url,
+  CART: PATH.CART.url,
+  MY: PATH.MY.url,
+  MY_PAGE: PATH.MY_PAGE.url,
+  COUNTRY_SETTING: PATH.COUNTRY_SETTING.url,
+  CHECKOUT: (checkoutRequestItems: string) =>
+    PATH.ORDER_CHECKOUT.url(checkoutRequestItems),
+  CHECKOUT_SUCCESS: PATH.ORDER_CHECKOUT_SUCCESS,
+  CHECKOUT_FAIL: PATH.ORDER_CHECKOUT_FAIL,
+  ORDERS_HISTORY: PATH.ORDER_HISTORY.url,
+  ORDERS_DETAIL: (orderId: number) => PATH.ORDER_DETAIL.url(orderId),
+  PRODUCT_DETAIL: (productId: number) => PATH.PRODUCT_DETAIL.url(productId),
+  PRODUCT_CATEGORY: (categoryId: string) =>
+    PATH.PRODUCT_CATEGORY.url(categoryId),
+  REVIEW_PRODUCT: (orderId: number, productId: number, productItemId: number) =>
+    PATH.REVIEW_PRODUCT.url(orderId, productId, productItemId),
+  ERROR_AUTHORIZED: PATH_ERROR.ERROR_AUTHORIZED,
+  ERROR_UNAUTHORIZED: PATH_ERROR.ERROR_UNAUTHORIZED,
+  ERROR_RESTRICTED: PATH_ERROR.ERROR_RESTRICTED,
 };

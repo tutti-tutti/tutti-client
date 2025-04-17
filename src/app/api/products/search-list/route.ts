03625b1ef7589d70551fa0f02d2b4c7862faf787
bbjbc <jobc4550@gmail.com>
2025-04-17 20:56:22 +0900

    refactor: api route에서 서비스함수 사용 및 에러 메시지와 에러 코드 실제의 에러대로 표기 (#312)

--- src/app/api/products/search-list/route.ts
+++ src/app/api/products/search-list/route.ts
@@ -1,25 +1,31 @@
 import { NextResponse } from 'next/server';
 
-import { axiosInstance } from '@/lib';
-import { PRODUCT_API_ROUTE_MESSAGE, PRODUCTS_ENDPOINTS } from '@/constants';
+import { PRODUCT_API_ROUTE_MESSAGE } from '@/constants';
+import { fetchSearchProductsWithPagination } from '@/services';
 
 export const POST = async (request: Request) => {
   try {
     const body = await request.json();
     const { keyword, cursorId, size } = body;
 
-    const response = await axiosInstance.post(PRODUCTS_ENDPOINTS.SEARCH, {
-      keyword: keyword || '',
-      cursorId: cursorId || null,
-      size: size || 20,
-    });
+    const response = await fetchSearchProductsWithPagination(
+      keyword || '',
+      cursorId || null,
+      size || 20,
+    );
 
-    return NextResponse.json(response.data);
+    return NextResponse.json(response);
   } catch (error) {
     console.error(PRODUCT_API_ROUTE_MESSAGE.SEARCH_PRODUCT_ERROR, error);
+
+    const status =
+      error instanceof Error && 'status' in error
+        ? (error.status as number)
+        : 500;
+
     return NextResponse.json(
-      { message: PRODUCT_API_ROUTE_MESSAGE.SEARCH_PRODUCT_ERROR },
-      { status: 500 },
+      { message: (error as Error).message },
+      { status: status },
     );
   }
 };
